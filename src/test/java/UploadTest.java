fbe14e25bff7a0ae420209e20b816f4ef04a3155
Judyta Odrobinska <judyta.odrobinska@gmail.com>
2024-07-16 17:29:58 +0200

    Screenshot

--- src/test/java/UploadTest.java
+++ src/test/java/UploadTest.java
@@ -1,10 +1,12 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.InvalidArgumentException;
-import org.openqa.selenium.WebDriver;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
 
 public class UploadTest {
 
@@ -22,16 +24,19 @@ public class UploadTest {
     }
 
     @Test
-    public void uploadFileTest() {
+    public void uploadFileTest() throws IOException {
 
         WebDriver driver = getDriver("chrome");
         driver.manage().window().maximize();
         driver.get("https://testeroprogramowania.github.io/selenium/fileupload.html");
 
-        // lokalizowanie
+        // lokalizowanie i ścieżka
         driver.findElement(By.id("myFile")).sendKeys("C:\\Users\\Judyta\\Downloads\\Judyta-Odrobinska-CV.pdf");
 
-
+        //screenshot
+        TakesScreenshot screenshot = (TakesScreenshot) driver;
+        File srcFile = screenshot.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(srcFile,new File("src/test/resources/screenshot.png")); // Make directory as Test Resources Root !!!
 
     }
 }

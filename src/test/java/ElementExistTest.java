b672cbc5c05bbf9a004d557a8f324dea3b21dcf5
Judyta Odrobinska <judyta.odrobinska@gmail.com>
2024-07-18 15:54:29 +0200

    czy element jest widoczny / czy element jest aktywny / czy element jest zaznaczony

--- src/test/java/ElementExistTest.java
+++ src/test/java/ElementExistTest.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.InvalidArgumentException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
@@ -32,12 +33,35 @@ public class ElementExistTest {
         driver.manage().window().maximize();
         driver.get("https://testeroprogramowania.github.io/selenium/basics.html");
 
+        // sprawdzenie czy element istnieje na stronie
         System.out.println(elementExist(By.tagName("p"))); //true
         System.out.println(elementExist(By.id("fname"))); //true
         System.out.println(elementExist(By.id("fnaame"))); //false
 
+        //-------------------
+
+        //spradzenie czy element jest widoczny dla użytkownika
+        System.out.println(driver.findElement(By.tagName("p")).isDisplayed()); //jest ukryty - false
+        System.out.println(driver.findElement(By.tagName("button")).isDisplayed()); // widoczny - true
+
+        //--------------------
+
+        //sprawdzenie czy element jest aktywny (czy wyszarzony)
+        System.out.println(driver.findElement(By.tagName("button")).isEnabled()); // aktywny - true
+
+        //---------------------
+
+        //sprawdzenie czy checkbox jest zaznaczony
+        System.out.println("==========");
+        WebElement checkboxInput = driver.findElement(By.cssSelector("[type='checkbox']"));
+        System.out.println(checkboxInput.isSelected());  //false - nie zaznaczony
+        checkboxInput.click();
+        System.out.println(checkboxInput.isSelected()); //true - zaznaczony
+
     }
 
+
+    //sprawdzenie czy element istnieje na stronie
     public boolean elementExist(By locator) {
 
         return driver.findElements(locator).size() > 0;

a161c246a72a79d8e4765e40672ac5334c0818a0
Judyta Odrobinska <judyta.odrobinska@gmail.com>
2024-07-18 18:24:48 +0200

    Metoda sleep

---
+++ src/test/java/WaitTest.java
@@ -0,0 +1,39 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.InvalidArgumentException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.Test;
+
+public class WaitTest {
+
+    WebDriver driver;
+
+    public WebDriver getDriver(String browser) {
+        switch (browser) {
+            case "chrome":
+                return new ChromeDriver();
+            case "firefox":
+                return new FirefoxDriver();
+            case "ie":
+                return new InternetExplorerDriver();
+            default:
+                throw new InvalidArgumentException("Invalid browser name");
+        }
+    }
+
+    @Test
+    public void waitTest() throws InterruptedException {
+
+        driver = getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.get("https://testeroprogramowania.github.io/selenium/wait2.html");
+
+        driver.findElement(By.id("clickOnMe")).click();
+        Thread.sleep(5000); // waiting 5s, unikać tej metody
+        driver.findElement(By.cssSelector("p"));
+
+    }
+}

fa16100b7bb87463fc23777090d734b6063d34f7
Judyta Odrobinska <judyta.odrobinska@gmail.com>
2024-07-22 18:09:59 +0200

    test listener

--- src/test/java/pl/testowanie/TestListener.java
+++ src/test/java/pl/testowanie/TestListener.java
@@ -1,10 +1,20 @@
 package pl.testowanie;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
+import java.io.File;
+import java.io.IOException;
+
 public class TestListener implements ITestListener {
+
+    WebDriver driver;
 
     @Override
     public void onTestStart(ITestResult iTestResult) {
@@ -18,7 +28,19 @@ public class TestListener implements ITestListener {
 
     @Override
     public void onTestFailure(ITestResult iTestResult) {
-        System.out.println("I am taking screenshot, it is fail");
+
+        driver = DriverFactory.getDriver();
+
+        int randomNumber = (int) (Math.random()*1000); // unikalna nazwa screenshota
+
+        TakesScreenshot screenshot = (TakesScreenshot) driver;
+        File before = screenshot.getScreenshotAs(OutputType.FILE);
+        String fileName = "failedTest" + randomNumber + ".png"; // unikalna nazwa screenshota before
+        try {
+            FileUtils.copyFile(before,new File("src/test/resources/" + fileName)); // Make directory as Test Resources Root !!!
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
